06602f768d1ce5b2a7ffbd58e8cd45bb3a08655d
Yasir Mohamed <muadnoor.27@gmail.com>
2023-04-04 10:11:56 -0500

    set up about template

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+
+
+function About() {
+    return(
+        <section className="about">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac scelerisque magna, sed tristique dui. Aliquam malesuada vel ante id volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla cursus posuere fermentum. Phasellus vel est non velit consequat elementum. Donec tempus est ut nisl semper, sit amet condimentum enim ultrices. Proin ac orci ac odio feugiat tincidunt. Mauris vehicula diam at nisi rhoncus, at vulputate diam facilisis.
+
+Aliquam nec magna vitae massa faucibus efficitur. Aenean maximus purus in ligula scelerisque efficitur. Aenean gravida, ipsum a mattis interdum, nisl velit egestas nulla, vitae luctus libero.</p>
+        </section>
+    )
+}
+
+export default About;
